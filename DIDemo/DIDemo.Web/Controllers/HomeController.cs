deefb7dbc99112a7c536029116ddb9258c1325d8
Adam Pope <adam@storm-consultancy.com>
2011-09-03 20:45:40 +0100

    Add example controllers

---
+++ DIDemo/DIDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DIDemo.Data;
+
+namespace DIDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMessage(string message)
+        {
+            var emailSvc = new EmailService();
+            emailSvc.Send(message);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DIDemo/DIDemo.Web/Controllers/HomeDIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DIDemo.Core;
+
+namespace DIDemo.Web.Controllers
+{
+    public class HomeDIController : Controller
+    {
+        private IMessagingService messageSvc;
+
+        public HomeDIController(IMessagingService msg)
+        {
+            this.messageSvc = msg;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMessage(string message)
+        {
+            this.messageSvc.Send(message);
+            return RedirectToAction("Index");
+        }
+    }
+}
